d7af17d42cbea6eafe1e55300662fb7e61a31cba
Marvin Gertler <marvingertler@gmail.com>
2026-06-19 08:01:42 +0000

    initial commit

---
+++ wg-monitor.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+WG_IF="wg0"
+NOW=$(date +%s)
+
+declare -A PEERS
+PEERS["10.10.10.2"]="Iphone 16 von Marvin"
+PEERS["10.10.10.3"]="Ubuntu-Client"
+PEERS["10.10.10.4"]="Handy von Andrea"
+PEERS["10.10.10.5"]="Marvin Win Laptop"
+PEERS["10.10.10.6"]="Laptop"
+PEERS["10.10.10.7"]="Jason Arbeitslaptop"
+PEERS["10.10.10.8"]="Jonathan Arbeitslaptop"
+PEERS["10.10.10.9"]="Jason Home-Rechner"
+PEERS["10.10.10.10"]="Test10"
+PEERS["10.10.10.11"]="Marvin Tablet"
+
+echo " "
+echo "WireGuard Status"
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+sudo wg show "$WG_IF" dump | tail -n +2 | while IFS=$'\t' read -r public_key preshared_key endpoint allowed_ips latest_handshake transfer_rx transfer_tx persistent_keepalive
+do
+    IP=$(echo "$allowed_ips" | cut -d'/' -f1)
+    NAME=${PEERS[$IP]:-"Unbekannt"}
+
+    if [ "$latest_handshake" -eq 0 ]; then
+        echo "[OFFLINE] $NAME ($IP) - noch kein Handshake"
+	echo " "
+        continue
+    fi
+
+    AGE=$((NOW - latest_handshake))
+
+    if [ "$AGE" -lt 300 ]; then
+        STATUS="OK"
+    elif [ "$AGE" -lt 3600 ]; then
+        STATUS="WARN"
+    else
+        STATUS="OFFLINE"
+    fi
+
+    if [ "$AGE" -lt 60 ]; then
+        AGE_TEXT="${AGE}s"
+    elif [ "$AGE" -lt 3600 ]; then
+        AGE_TEXT="$((AGE / 60))min"
+    elif [ "$AGE" -lt 86400 ]; then
+        AGE_TEXT="$((AGE / 3600))h"
+    else
+        AGE_TEXT="$((AGE / 86400))d"
+    fi
+
+    echo "[$STATUS] $NAME ($IP) - letzter Handshake vor $AGE_TEXT" 
+    echo " "
+
+done
